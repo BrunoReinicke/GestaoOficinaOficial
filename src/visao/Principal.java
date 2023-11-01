52eef6e33ae3d25035f2cda478d66e44f12e636b
Bruno Reinicke <Bruno Reinicke@DESKTOP-MNN4OO1>
2023-10-31 23:03:43 -0300

    Ajuste do nome do sistema.

--- src/visao/Principal.java
+++ src/visao/Principal.java
@@ -325,7 +325,7 @@ public class Principal extends javax.swing.JFrame {
         });
 
         jLabel1.setFont(new java.awt.Font("Segoe UI Black", 1, 16)); // NOI18N
-        jLabel1.setText("Sistema para controle de OS");
+        jLabel1.setText("Sistema para Controle de OS");
 
         jLabel2.setToolTipText("");
 
@@ -335,7 +335,7 @@ public class Principal extends javax.swing.JFrame {
         jSeparator1.setMinimumSize(new java.awt.Dimension(50, 20));
         jSeparator1.setOpaque(true);
 
-        jLabel4.setIcon(new javax.swing.ImageIcon("C:\\Users\\Bruno Reinicke\\Pictures\\Oficina Reinicke 700.jpg")); // NOI18N
+        jLabel4.setIcon(new javax.swing.ImageIcon("C:\\Users\\Bruno Reinicke\\Pictures\\Oficina Reinicke 650.jpg")); // NOI18N
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -388,7 +388,7 @@ public class Principal extends javax.swing.JFrame {
                                 .addGap(1106, 1106, 1106)
                                 .addComponent(jLabel3))
                             .addGroup(layout.createSequentialGroup()
-                                .addGap(62, 62, 62)
+                                .addGap(80, 80, 80)
                                 .addComponent(jLabel4))))
                     .addComponent(jSeparator1, javax.swing.GroupLayout.DEFAULT_SIZE, 1381, Short.MAX_VALUE))
                 .addContainerGap(27, Short.MAX_VALUE))
@@ -428,10 +428,13 @@ public class Principal extends javax.swing.JFrame {
                 .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 2, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabel3)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 525, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel4))
+                    .addGroup(layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 525, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(28, 28, 28)
+                        .addComponent(jLabel4)))
                 .addGap(12, 12, 12))
         );
 
